55750f60207e9ae355e7a0e03e39ce43d2a10a5c
Christopher de Sousa <cdesousa89@gmail.com>
2017-08-23 10:14:09 -0700

    linter, lint npm script

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode....'));
-console.log(chalk.yellow('Node Security Check has been disabled...'));
+console.log(chalk.green('Starting app in dev mode....')); // eslint-disable-line no-console
+console.log(chalk.yellow('Node Security Check has been disabled...')); // eslint-disable-line no-console 
