a2fb5f88ca2543480d10fa16d5dfae0bc777e3cf
Christopher de Sousa <cdesousa89@gmail.com>
2017-08-23 11:55:35 -0700

    index.html testing

---
+++ src/index.test.js
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', () => {
+    it('should say hello', () => {
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+    })
+});
